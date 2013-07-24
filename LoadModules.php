925ca606d95f628291067061de41d9d9654fd3c3
slipperyNipple <slipperyNipple@flenstad.com>
2013-07-24 19:19:54 +0200

    Loads modules

---
+++ LoadModules.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: thofle
+ * Date: 24.07.13
+ * Time: 19:12
+ */
+
+// TODO: Load modules dynamically
+require_once('./modules/mod_RESTART.php');
+require_once('./modules/mod_STATUS.php');
