ae270c2b248449059f59807b7cfa6776a0f12d74
slipperyNipple <slipperyNipple@flenstad.com>
2013-07-25 19:28:45 +0200

    Added logging

--- modules/mod_AUTH.php
+++ modules/mod_AUTH.php
@@ -10,7 +10,12 @@ function executeCommandAUTH(&$SMSGW, $argument = null)
   $code = $SMSGW->getAndSaveAuthCode();
 
   if ($code > 9999 && $code < 100000)
+  {
     return $SMSGW->sendMessage('Auth code: ' . $code);
+  }
   else
+  {
+    $SMSGW->log(__FUNCTION__, 'Unable to save auth code to db or invalid code - ' . $code);
     return false;
+  }
 }
