5dd5cbc6c6d2bb92725929f6ec6c8caa3fc742f4
slipperyNipple <slipperyNipple@flenstad.com>
2013-07-25 19:37:02 +0200

    Intiial commit, added CATFACTS

---
+++ modules/mod_STOP.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: thofle
+ * Date: 25.07.13
+ * Time: 19:23
+ */
+
+
+function executeCommandSTOP(&$SMSGW, $argument = null)
+{
+  list($mainArgument, $secondaryArgumens) = $SMSGW->parseIncomingMessage($argument);
+
+  if($mainArgument != null)
+  {
+    switch ($mainArgument)
+    {
+      case 'CATFACTS':
+      case 'CATFACT':
+        $SMSGW->sendMessage('Thanks for subscribing to daily cat facts! Send STOP CATFACTS to stop.');
+        require_once('./extensions/ext_CATFACTS.php');
+        return $SMSGW->sendMessage(extentsionGetCatFact($SMSGW->pDB));
+
+      default:
+        $this->log(__FUNCTION__, 'Undefined mainArgument - ' . $mainArgument);
+        $SMSGW->error = 'Undefined mainArgument';
+        return false;
+    }
+  }
+
+  $this->log(__FUNCTION__, 'Invalid mainArgument');
+  $SMSGW->error = 'Invalid mainArgument';
+
+  return false;
+}
